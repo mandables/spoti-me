47efc1bb3c91bc4114b144ef4c7d872e024fb76e
Mani <amaan.ahmed3@gmail.com>
2020-01-21 08:25:04 +0000

    Added material table into Results. Using dummy data

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,25 +1,64 @@
 import React from "react";
 import ResultsRow from "./ResultsRow";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
+console.log("hello");
 
 function Results(props) {
-  let rows = () => {
-    if (props.results) {
-      return props.results.map(result => {
-        return <ResultsRow info={result} />;
-      });
+  const useStyles = makeStyles({
+    table: {
+      minWidth: 650
     }
-  };
+  });
+
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  }
+
+  const rows = [
+    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+    createData("Eclair", 262, 16.0, 24, 6.0),
+    createData("Cupcake", 305, 3.7, 67, 4.3),
+    createData("Gingerbread", 356, 16.0, 49, 3.9)
+  ];
+
+  const classes = useStyles();
+
   return (
-    <div>
-      <table>
-        <tr>
-          <th>Song</th>
-          <th>Artist</th>
-          <th>Album</th>
-        </tr>
-        {rows()}
-      </table>
-    </div>
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Song</TableCell>
+            <TableCell align="right">Artist</TableCell>
+            <TableCell align="right">Album</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
+
 export default Results;
--- src/components/ResultsRow.js
+++ src/components/ResultsRow.js
@@ -3,13 +3,11 @@ import React from "react";
 function ResultsRow(props) {
   const { name, artist, album } = props.info;
   return (
-    <div>
-      <tr>
-        <td>{name}</td>
-        <td>{artist}</td>
-        <td>{album}</td>
-      </tr>
-    </div>
+    <tr>
+      <td>{name}</td>
+      <td>{artist}</td>
+      <td>{album}</td>
+    </tr>
   );
 }
 export default ResultsRow;
