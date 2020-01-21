bc1a538f3ee028834b1ac8a56bd7e3c47151eb9e
Mani <amaan.ahmed3@gmail.com>
2020-01-21 16:13:26 +0000

    Added user auth links and tested them

--- src/adapters/API.js
+++ src/adapters/API.js
@@ -1,9 +1,13 @@
 const TOKEN_URL = "https://accounts.spotify.com/api/token";
 const BASE_URL = "https://api.spotify.com/v1/";
+const USER_AUTH = "https://accounts.spotify.com/authorize"
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+const AUTH_QUERY = "https://accounts.spotify.com/authorize?client_id=d31f37a814f743b8934138cb93e7ae57&response_type=code&redirect_uri=http://localhost:3000&scope=user-read-private%20user-read-email"
 
 export default class API {
+
+
   static getToken = () => {
     let data = new URLSearchParams();
     data.append("client_id", CLIENT_ID);
@@ -22,4 +26,5 @@ export default class API {
       headers: { Authorization: "Bearer " + token }
     }).then(resp => resp.json());
   };
+
 }
