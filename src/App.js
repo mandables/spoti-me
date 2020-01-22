53eb44cd7504652da438676699695130a0c905ee
Mani <amaan.ahmed3@gmail.com>
2020-01-22 10:19:00 +0000

    Refactored with router, implemented playlists route

--- src/App.js
+++ src/App.js
@@ -3,24 +3,17 @@ import "./App.scss";
 import API from "./adapters/API";
 import Results from "./components/Results";
 import Search from "./components/Search";
+import { Router } from "@reach/router";
+import Playlists from "./pages/Playlists";
 
 export default class App extends Component {
-  state = {
-    results: ""
-  };
-
-  setResults = results => {
-    this.setState({
-      results
-    });
-  };
-
   render() {
     return (
-      <div className="App">
-        <Search setResults={this.setResults} />
-        {this.state.results ? <Results results={this.state.results} /> : null}
-      </div>
+      <>
+        <Router>
+          <Playlists path="/playlists" />
+        </Router>
+      </>
     );
   }
 }
