cc2577330a9f807a0d70edf0105c8aaa606fb479
Mani <amaan.ahmed3@gmail.com>
2020-01-28 09:50:33 +0000

    Added bg image to playlists

--- src/components/Callback.js
+++ src/components/Callback.js
@@ -3,14 +3,14 @@ import API from "../adapters/API";
 import { Redirect } from "@reach/router";
 
 function Callback(props) {
-  useEffect(() => setToken(), []);
-
   let setToken = () => {
     let code = props.location.href.split("code=")[1];
     API.getUserToken(code).then(resp => {
       localStorage.setItem("r_token", resp.refresh_token);
     });
   };
+
+  useEffect(() => setToken(), []);
 
   return <Redirect noThrow to="/playlists" />;
 }
