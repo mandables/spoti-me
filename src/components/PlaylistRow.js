c023c7c2bac4f614b0e11a7e7057d72f2ddff6f0
Mani <amaan.ahmed3@gmail.com>
2020-01-30 18:01:25 +0000

    Played with responsiveness. Needs work. Added icons and hover CSS

--- src/components/PlaylistRow.js
+++ src/components/PlaylistRow.js
@@ -1,18 +1,33 @@
-import React from "react";
-import { truncate} from "../utils/helpers";
+import React, { useState } from "react";
+import { truncate } from "../utils/helpers";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faMinus } from "@fortawesome/free-solid-svg-icons";
+
+const check = <FontAwesomeIcon icon={faCheck} />;
+const minus = <FontAwesomeIcon icon={faMinus} />;
 
 function PlaylistRow(props) {
   const { name, id } = props.info;
+  const [selected, setSelected] = useState(false);
+
+  const renderSelectIcon = id => {
+    props.selectPlaylist(id);
+    setSelected(!selected);
+  };
 
   return (
     <tr>
       <td>
         {truncate(name)}
-        <input
+        {/* <input
           onClick={() => props.selectPlaylist(id)}
           value={id}
           type="checkbox"
-        />
+        /> */}
+        {/* <p id="selector">{element}</p> */}
+        <p onClick={() => renderSelectIcon(id)} id="selector">
+          {selected ? check : minus}
+        </p>
       </td>
     </tr>
   );
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,5 +1,5 @@
 export {truncate}
 
 function truncate(string) {
-    return string.length > 20 ? string.slice(0, 20) + "..." : string;
+    return string.length > 15 ? string.slice(0, 13) + "..." : string;
   };
